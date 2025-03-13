2dc6dbdf9c66061484b0c96ea683209c86fbd229
Butsakon127 <42908@prot.ac.th>
2025-03-13 11:48:24 +0700

    Add files via upload

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,13 @@
+import sys
+if len(sys.argv) == 2:
+    inpot_string = sys.argv[1]
+    count_z = input_string.count('z')
+    if count_z > 0:
+        for _ in range(count_z):
+            print("z")
+    else:
+        print("none")
+
+else:
+    print("none")
+    
